71de945a5b4600ee46dffab6672304be5689f434
C.Gunasekar <137645060+guna-design@users.noreply.github.com>
2024-08-21 17:15:24 +0530

    book temp img

--- src/home/BannerCard.jsx
+++ src/home/BannerCard.jsx
@@ -20,14 +20,14 @@ const BannerCard = () => {
     modules={[EffectCards]}
     className="mySwiper"
   >
-    <SwiperSlide></SwiperSlide>
-    <SwiperSlide></SwiperSlide>
-    <SwiperSlide></SwiperSlide>
-    <SwiperSlide></SwiperSlide>
-    <SwiperSlide></SwiperSlide>
-    <SwiperSlide></SwiperSlide>
-    <SwiperSlide></SwiperSlide>
-    <SwiperSlide></SwiperSlide>
+    <SwiperSlide><img src="https://www.creativindiecovers.com/wp-content/gallery/portfolio/sherifinal.jpg" alt="" srcset="" /></SwiperSlide>
+    <SwiperSlide><img src="https://www.creativindiecovers.com/wp-content/gallery/portfolio/colorsfinal2.jpg" alt="" srcset="" /></SwiperSlide>
+    <SwiperSlide><img src="https://www.ingramspark.com/hs-fs/hubfs/TheSumofAllThings_cover_June21_option4(1).jpg?width=1125&name=TheSumofAllThings_cover_June21_option4(1).jpg" alt="" srcset="" /></SwiperSlide>
+    <SwiperSlide><img src="https://bespokebookcovers.com/wp-content/uploads/2016/09/paperback-cover-design-examples.jpg" alt="" /></SwiperSlide>
+    <SwiperSlide><img src="https://images.template.net/106903/free-vintage-book-signing-poster-8pxm9.jpg" alt="" /></SwiperSlide>
+    <SwiperSlide><img src="https://www.creativindiecovers.com/wp-content/gallery/portfolio/28E.jpg" alt="" /></SwiperSlide>
+    <SwiperSlide><img src="https://www.creativindiecovers.com/wp-content/gallery/portfolio/budapestfinal2.jpg" alt="" /></SwiperSlide>
+    <SwiperSlide><img src="https://www.creativindiecovers.com/wp-content/gallery/portfolio/proposalnew4.jpg" alt="" /></SwiperSlide>
  
   </Swiper></div>
   )
